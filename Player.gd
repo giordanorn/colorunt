c56290751e222b8fac13de7f8498fe815b3919dd
Giordano Vicente <22085029+giordanorn@users.noreply.github.com>
2020-12-13 20:21:39 -0300

    Movimentação do player para a direita apenas

--- Player.gd
+++ Player.gd
@@ -3,26 +3,48 @@ extends KinematicBody
 
 export var speed:int = 1
 
-var direction = Vector3.ZERO
 var initial_position
+var direction:Vector3 = Vector3.ZERO
+
+const DISTANCE:float = 2.0
+var distance_remaining = DISTANCE
+var pos:Vector3
+
+var moving:bool = false
 
 func _ready():
-	initial_position=translation
-	#$MeshInstance.get_surface_material(0).albedo_color = Color(1,0,1)
-	pass
+	initial_position = translation
 
 
 func _process(delta) -> void:
-	if Input.is_action_pressed("ui_up"):
-		direction = Vector3.UP
-	elif Input.is_action_pressed("ui_left"):
-		direction = Vector3.LEFT
-	elif Input.is_action_pressed("ui_right"):
-		direction = Vector3.RIGHT
-	elif Input.is_action_pressed("ui_down"):
-		direction = Vector3.DOWN
+	if moving:
+		move(delta)
 	else:
-		direction = Vector3.ZERO
+		distance_remaining = DISTANCE
+		pos = translation
+		if set_move_direction():
+			moving = true
+
+
+func set_move_direction() -> bool:
+	direction = Vector3.ZERO
+	if Input.is_action_pressed("ui_up"):
+		direction += Vector3.UP
+	if Input.is_action_pressed("ui_left"):
+		direction += Vector3.LEFT
+	if Input.is_action_pressed("ui_right"):
+		direction += Vector3.RIGHT
+	if Input.is_action_pressed("ui_down"):
+		direction += Vector3.DOWN
 	
+	return direction != Vector3.ZERO
+
+
+
+
+func move(delta) -> void:
 	var velocity = ((speed * delta) * direction)
 	move_and_collide(velocity)
+	
+	if (translation.x - pos.x >= DISTANCE):
+		moving = false
