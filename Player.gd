495432151c8a7cca8fdecf0b28830715ebb47b2d
Giordano Vicente <22085029+giordanorn@users.noreply.github.com>
2020-12-13 17:03:17 -0300

    Import do projeto base

---
+++ Player.gd
@@ -0,0 +1,24 @@
+extends RigidBody
+
+
+export var speed:int = 1
+
+var direction = Vector3.ZERO
+
+
+func _process(delta) -> void:
+	if Input.is_action_pressed("ui_up"):
+		direction = Vector3.UP
+	elif Input.is_action_pressed("ui_left"):
+		direction = Vector3.LEFT
+	elif Input.is_action_pressed("ui_right"):
+		direction = Vector3.RIGHT
+	elif Input.is_action_pressed("ui_down"):
+		direction = Vector3.DOWN
+	else:
+		direction = Vector3.ZERO
+	
+	move(delta)
+
+func move(delta) -> void:
+	translation = ((speed * delta) * direction) + translation
